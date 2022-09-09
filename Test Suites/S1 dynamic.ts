94773c910e4d7d9fd469415722f25f2425f1350e
DESKTOP-EJO76L4\tuyen.nguyen <tuyen.nguyen@katalon.com>
2022-09-09 16:06:27 +0700

    ahihi

---
+++ Test Suites/S1 dynamic.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>S1 dynamic</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a8c4d46e-d95b-44b9-b433-9d83082f7544</testSuiteGuid>
+   <filteringBuiltIn></filteringBuiltIn>
+   <filteringExtension>com.katalon.plugin.dynamic_execution.TestRailFilteringTestSuiteImpl</filteringExtension>
+   <filteringPlugin>com.katalon.katalon-studio-testrail-plugin</filteringPlugin>
+   <filteringText>R1</filteringText>
+</FilteringTestSuiteEntity>
